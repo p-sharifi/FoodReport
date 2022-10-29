62ea7e9d1eb5283ccf9a5394c301079202296aa0
nima soufiloo <nima.soufiloo@gmail.com>
2022-10-29 21:57:59 +0330

    initial commit

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,24 @@
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
+import colors from "vuetify/lib/util/colors";
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.cyan.darken3,
+        btnPrimary: colors.cyan.darken1,
+        mainBackgroundColor: colors.grey.lighten2,
+        backgroundColor: colors.grey.lighten4,
+      },
+      dark: {
+        primary: colors.grey.lighten1,
+        btnPrimary: colors.grey.darken2,
+        mainBackgroundColor: colors.grey.darken4,
+        backgroundColor: colors.grey.darken3,
+      },
+    },
+  },
+});
