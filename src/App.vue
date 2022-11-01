fe153dd5737e514bf299e3d68d7ab66c4d1381b2
Parisa Sharifi <parisa.sharifi.1373@gmail.com>
2022-11-01 16:55:23 +0330

    add slider

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,40 @@
 
     <v-main>
       <router-view />
+
     </v-main>
   </v-app>
 </template>
+<style>
+@font-face {
+  font-family: "IRAN Sans";
+  src: url("assets/Css/IRANSans-Regular.otf") format("otd");
+}
+div {
+  font-family: "IRAN Sans";
+}
+</style>
 
 <script>
 export default {
-  name: "App",
+    data: () => ({ isMobile: false }),
 
-  data: () => ({
-    //
-  }),
-};
+    beforeDestroy () {
+      if (typeof window === 'undefined') return
+
+      window.removeEventListener('resize', this.onResize, { passive: true })
+    },
+
+    mounted () {
+      this.onResize()
+
+      window.addEventListener('resize', this.onResize, { passive: true })
+    },
+
+    methods: {
+      onResize () {
+        this.isMobile = window.innerWidth < 600
+      },
+    },
+  }
 </script>
