03c0b859ee7d3df7a6db6d58bcaf85e822aeb271
Parisa Sharifi <parisa.sharifi.1373@gmail.com>
2022-11-05 17:56:44 +0330

    third edit on first page

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,94 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark> </v-app-bar>
 
+    <v-card
+        class="flex"
+        color="blue darken-4"
+        flat
+        tile
+        elevation="15"
+        dark
+        height="55"
+    >
+      <v-card-title
+          class="justify-xl-center"
+      >
+        <strong class="justify-space-between">
+          راهنمای سامانه های رفع مغایرت بانک مرکزی
+        </strong>
+      </v-card-title>
+
+    </v-card>
+    <v-spacer></v-spacer>
     <v-main>
-      <router-view />
+      <router-view/>
 
     </v-main>
+    <v-footer
+        dark
+        padless
+
+    >
+      <v-card
+          class="flex"
+          color="blue darken-4"
+          flat
+          tile
+          elevation="15"
+          height="55"
+      >
+        <v-card-title
+            class="justify-xl-center"
+        >
+          <strong class="justify-space-between">
+            در صورت بروز هر گونه مشکل در سامانه با شماره 02172351500 در تماس باشید.
+            <strong>شرکت خدمات انفورماتیک</strong>
+            <v-spacer></v-spacer>
+          </strong>
+        </v-card-title>
+
+      </v-card>
+    </v-footer>
   </v-app>
+
 </template>
 <style>
+
 @font-face {
   font-family: "IRAN Sans";
   src: url("assets/Css/IRANSans-Regular.otf") format("otd");
 }
+
 div {
   font-family: "IRAN Sans";
 }
+
+
 </style>
 
 <script>
 export default {
-    data: () => ({ isMobile: false }),
+  name: 'app',
+  data: () => ({
+    isMobile: false
+  }),
 
-    beforeDestroy () {
-      if (typeof window === 'undefined') return
+  beforeDestroy() {
+    if (typeof window === 'undefined') return
 
-      window.removeEventListener('resize', this.onResize, { passive: true })
+    window.removeEventListener('resize', this.onResize, {passive: true})
+  },
+
+  mounted() {
+    this.onResize()
+
+    window.addEventListener('resize', this.onResize, {passive: true})
+  },
+
+  methods: {
+    onResize() {
+      this.isMobile = window.innerWidth < 600
     },
-
-    mounted () {
-      this.onResize()
-
-      window.addEventListener('resize', this.onResize, { passive: true })
-    },
-
-    methods: {
-      onResize () {
-        this.isMobile = window.innerWidth < 600
-      },
-    },
-  }
+  },
+}
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,119 +1,228 @@
 <template>
-  <v-container >
+  <v-container
+      class="app"
+  >
     <v-row class="text-center"
     >
 
-      <v-col co ls="12" >
-        <v-img
-            class="mx-auto"
-            max-width="100"
-            max-height="200"
-          :src="require('../assets/Logo/logo.png')"
-          contain
-          height="80"
-        />
-
-      </v-col >
-      <v-col >
-      <v-card color="test"
-      class="ma-lg-16"
-      max-width="390"
-
+      <v-col cols="12"
       >
-        <v-img
-        :src="require('../assets/Images/ساتنا.jpg')"
-        height="200px"
-        >
-        </v-img>
-      <v-card-title class="justify-xl-center">
-        رفع مغایرت ساتنا
-      </v-card-title>
-        <v-card-subtitle class="font-weight-bold">
-          راهنمای کاربری سامانه ساتنا
-        </v-card-subtitle>
-        <v-card-actions>
-          <v-btn
-          color="blue"
-          text
-          >
-            راهنما
-          </v-btn>
-          <v-spacer>
-          </v-spacer>
-          <v-btn
-              icon
-              @click="show = !show"
-          >
-            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-          </v-btn>
-        </v-card-actions>
-        <v-expand-transition>
-          <div
-          v-show="show"
-          >
-            <v-divider></v-divider>
-            <v-card-text>
-              عودت و استرداد اشتباه واریزی و موارد مختلف مغایرت بین بانک ها از طریق این سامانه قابل انجام و پیگیری میباشد
-            </v-card-text>
-
-          </div>
-        </v-expand-transition>
-
-      </v-card>
-      </v-col>
-      <v-col >
-        <v-card
-            class="ma-lg-16"
-            max-width="390"
-        >
+        <v-row>
           <v-img
-              :src="require('../assets/Images/ساتنا.jpg')"
-              height="200px"
-          >
-          </v-img>
-          <v-card-title class="justify-xl-center">
-            رفع مغایرت ساتنا
-          </v-card-title>
-          <v-card-subtitle class="font-weight-bold">
-            راهنمای کاربری سامانه ساتنا
-          </v-card-subtitle>
-          <v-card-actions>
-            <v-btn
-                color="blue"
-                text
-            >
-              راهنما
-            </v-btn>
-            <v-spacer>
-            </v-spacer>
-            <v-btn
-                icon
-                @click="show1 = !show1"
-            >
-              <v-icon>{{ show1 ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-            </v-btn>
-          </v-card-actions>
-          <v-expand-transition>
-            <div
-                v-show="show1"
-            >
-              <v-divider></v-divider>
-              <v-card-text>
-                عودت و استرداد اشتباه واریزی و موارد مختلف مغایرت بین بانک ها از طریق این سامانه قابل انجام و پیگیری میباشد
-              </v-card-text>
-
-            </div>
-          </v-expand-transition>
-
-        </v-card>
+              class="mx-auto"
+              max-width="200"
+              max-height="200"
+              :src="require('../assets/Logo/logo.png')"
+              contain
+              height="80"
+          />
+        </v-row>
       </v-col>
+      <v-row>
+        <v-col cols="12">
+          <v-card color="basil"
+                  class="ma-lg-13"
+                  max-width="390"
+          >
+            <v-img
+                :src="require('../assets/Images/ساتنا.jpg')"
+                height="200px"
+            >
+            </v-img>
+            <v-card-title class="justify-xl-center">
+              رفع مغایرت ساتنا
+            </v-card-title>
+            <v-card-subtitle class="font-weight-bold">
+              راهنمای کاربری سامانه ساتنا
+            </v-card-subtitle>
+            <v-card-actions>
+              <v-btn
+                  color="blue"
+                  text
+              >
+                راهنما
+              </v-btn>
+              <v-spacer>
+              </v-spacer>
+              <v-btn
+                  icon
+                  @click="show = !show"
+              >
+                <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+              </v-btn>
+            </v-card-actions>
+            <v-expand-transition>
+              <div
+                  v-show="show"
+              >
+                <v-divider></v-divider>
+                <v-card-text>
+                  عودت و استرداد اشتباه واریزی و موارد مختلف مغایرت ساتنا بین بانک ها
+                </v-card-text>
+
+              </div>
+            </v-expand-transition>
+
+          </v-card>
+
+          <v-card
+              color="basil"
+              class="ma-xl-13"
+              max-width="390"
+          >
+            <v-img
+                :src="require('../assets/Images/Samane-Pol.jpg')"
+                height="200px"
+            >
+            </v-img>
+            <v-card-title class="justify-xl-center">
+              رفع مغایرت پل
+            </v-card-title>
+            <v-card-subtitle class="font-weight-bold">
+              راهنمای کاربری سامانه پل
+            </v-card-subtitle>
+            <v-card-actions>
+              <v-btn
+                  color="blue"
+                  text
+              >
+                راهنما
+              </v-btn>
+              <v-spacer>
+              </v-spacer>
+              <v-btn
+                  icon
+                  @click="show2 = !show2"
+              >
+                <v-icon>{{ show2 ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+              </v-btn>
+            </v-card-actions>
+            <v-expand-transition>
+              <div
+                  v-show="show2"
+              >
+                <v-divider></v-divider>
+                <v-card-text>
+                  عودت و استرداد اشتباه واریزی و موارد مختلف مغایرت پل بین بانک ها
+                </v-card-text>
+
+              </div>
+            </v-expand-transition>
+
+          </v-card>
+
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="auto">
+
+          <v-card color="basil"
+                  class="ma-lg-13"
+                  max-width="390"
+
+          >
+            <v-img
+                :src="require('../assets/Images/Paya.png')"
+                height="200px"
+            >
+            </v-img>
+            <v-card-title class="justify-xl-center">
+              رفع مغایرت پایا
+            </v-card-title>
+            <v-card-subtitle class="font-weight-bold">
+              راهنمای کاربری سامانه پایا
+            </v-card-subtitle>
+            <v-card-actions>
+              <v-btn
+                  color="blue"
+                  text
+              >
+                راهنما
+              </v-btn>
+              <v-spacer>
+              </v-spacer>
+              <v-btn
+                  icon
+                  @click="show3 = !show3"
+              >
+                <v-icon>{{ show3 ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+              </v-btn>
+            </v-card-actions>
+            <v-expand-transition>
+              <div
+                  v-show="show3"
+              >
+                <v-divider></v-divider>
+                <v-card-text>
+                  عودت و استرداد اشتباه واریزی و موارد مختلف مغایرت پایا بین بانک ها
+                </v-card-text>
+
+              </div>
+            </v-expand-transition>
+
+          </v-card>
+          <v-card
+              color="basil"
+              class="ma-xl-13"
+              max-width="390"
+          >
+            <v-img
+                :src="require('../assets/Images/چکاوک.jpg')"
+                height="200px"
+            >
+            </v-img>
+            <v-card-title class="justify-xl-center">
+              رفع مغایرت چکاوک
+            </v-card-title>
+            <v-card-subtitle class="font-weight-bold">
+              راهنمای کاربری سامانه چکاوک
+            </v-card-subtitle>
+            <v-card-actions>
+              <v-btn
+                  color="blue"
+                  text
+              >
+                راهنما
+              </v-btn>
+              <v-spacer>
+              </v-spacer>
+              <v-btn
+                  icon
+                  @click="show1 = !show1"
+              >
+                <v-icon>{{ show1 ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+              </v-btn>
+            </v-card-actions>
+            <v-expand-transition>
+              <div
+                  v-show="show1"
+              >
+                <v-divider></v-divider>
+                <v-card-text>
+                  عودت و استرداد اشتباه واریزی و موارد مختلف مغایرت چکاوک بین بانک ها
+                </v-card-text>
+
+              </div>
+            </v-expand-transition>
+
+          </v-card>
+        </v-col>
+      </v-row>
+
+
     </v-row>
   </v-container>
 
 </template>
-<style scoped>
-.test{
-  background-color:snow;
+<style>
+.basil {
+  background-color: #E3F2FD !important;
+}
+
+.app {
+  background: url('../assets/Images/CenteralBank.png');
+  background-size: cover;
+  background-origin: unset;
 }
 
 
@@ -122,12 +231,14 @@
 export default {
   name: "HelloWorld",
 
-  data : () => ({
+  data: () => ({
+    show: false,
     show1: false,
-    show : false,
+    show2: false,
+    show3: false,
   })
 
-  }
+}
 
 
 </script>
